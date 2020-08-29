7148f2242009f939fd88d2d03f4d44ad8eaf73c0
Drake Stephens <meuio56@gmail.com>
2020-08-29 13:05:27 -0600

    done:

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commmentRoutes = require('./comment-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commmentRoutes);
 
 module.exports = router;
